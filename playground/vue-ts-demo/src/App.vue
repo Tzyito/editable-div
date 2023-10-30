7865bc8db093d30441a9008c5613cbb7c177f398
tzyito <568063539@qq.com>
2023-10-30 09:50:18 +0800

    docs: upgrad the demo

--- playground/vue-ts-demo/src/App.vue
+++ playground/vue-ts-demo/src/App.vue
@@ -1,18 +1,53 @@
 <script lang="ts" setup>
-import { ref } from "vue";
+import { reactive, ref } from "vue";
 import EditableDiv from "editable-div";
 const content = ref("");
+const states = reactive({
+  autogrow: false,
+  type: "input",
+  renderHtml: false,
+  canEdit: true,
+});
+const limit = ref(10);
+const renderHtml = ref("");
+const handleSwitch = (key: string, value: any = null) => {
+  return (states[key] = value ? value : !states[key]);
+};
+const resultText = ref("");
 </script>
 <template>
   <div>
-    <EditableDiv class="container" :content="content" :limit="10"></EditableDiv>
-    <p>输入内容：{{ content }}</p>
+    <div style="margin-bottom: 10px">
+      <button @click="handleSwitch('autogrow')">check autogrow</button>
+      <button @click="handleSwitch('type', 'textarea')">check type</button>
+      <button @click="handleSwitch('renderHtml')">check renderHtml</button>
+      <button @click="handleSwitch('canEdit')">check canEdit</button>
+      max word: <input type="number" v-model="limit" />
+      <div style="margin-top: 10px">
+        render html string:
+        <textarea type="textarea" v-model="renderHtml" style="width: 300px" />
+        <button @click="() => (content = renderHtml)">start render</button>
+      </div>
+    </div>
+    <EditableDiv
+      class="container"
+      :content="content"
+      :limit="limit"
+      @update:content="(s) => (resultText = s)"
+      v-bind="states"
+    ></EditableDiv>
+    {{ resultText }}
+    <div>
+      <li v-for="(key, index) in Object.keys(states)" :key="index">
+        current {{ key }}:
+        <span style="color: blueviolet"> {{ states[key] }}</span>
+      </li>
+    </div>
   </div>
 </template>
 <style>
 .container {
   background: #fff;
-  height: 100px;
   width: 200px;
 }
 </style>
